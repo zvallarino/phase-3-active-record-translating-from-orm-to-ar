27df563fd7eb7d2af1e8ffea9be255f79a388681
Danny-Sullivan <dsully360@gmail.com>
2015-11-20 10:51:53 -0500

    removed duplicate sql_runner

--- bin/environment.rb
+++ bin/environment.rb
@@ -1,11 +1,18 @@
 require 'bundler/setup'
 Bundler.require
-
+require 'rake'
+require 'active_record'
 
 require_relative '../lib/dog'
 require_relative 'sql_runner'
 
-ActiveRecord::Base.establish_connection(
+DB = ActiveRecord::Base.establish_connection(
       :adapter => "sqlite3",
       :database => "../db/dogs"
     )
+
+  DB = ActiveRecord::Base.connection
+
+  if ENV["ACTIVE_RECORD_ENV"] == "test"
+    ActiveRecord::Migration.verbose = false
+  end
